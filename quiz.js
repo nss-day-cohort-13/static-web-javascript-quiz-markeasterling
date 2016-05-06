e1b1c76432c07db5c398efee0efc1509311322f8
Mark Easterling <marke.easterling@gmail.com>
2016-05-06 09:47:23 -0500

    treegrow function now receives values from function, proper event listeners added

--- quiz.js
+++ quiz.js
@@ -1,37 +1,39 @@
-var rows;
-var treeBit;
+// object in which height and character is kept
 var treeGrowValues = {};
-
 
 // function that creates a tree based on the user's criteria of height and character choice
 	var treeGrow = function (rows , treeBit) {
-	event.preventDefault();
+	//event.preventDefault();
 
 // puts the user's form inputs into an object
 	treeGrowValues.rows = document.getElementById('howHigh').value;
 	treeGrowValues.treebit = document.getElementById('character').value;
 
-//defines variables used in the tree builder by retreiving strings from the object
-	rows = treeGrowValues.rows;
-	treeBit = treeGrowValues.treebit;
-
 // assures the user has entered a value for both fields. alert will notify user if a field is blank
-	if (rows=="" || isNaN(rows) || treeBit=="") {
+	if (treeGrowValues.rows=="" || isNaN(treeGrowValues.rows) || treeGrowValues.treebit=="") {
 		alert("Please enter an appropriate value for both fields.")
 	} else {
 
 
 // loop to build tree with repeating character treeBit
 	var row = 1;
-	for (var i = rows; i > 0; i--) {
-		string = ' '.repeat(i-1) + treeBit.repeat(row*2 - 1)
+	for (var i = treeGrowValues.rows; i > 0; i--) {
+		string = ' '.repeat(i-1) + treeGrowValues.treebit.repeat(row*2 - 1)
 		row++
 		console.log(string)
 	}
 	}
-}
+};
 
 // event listener. when "Grow your Tree!" is clicked, or the enter key is pressed with either
 // field focused, the treegrow function will run with the user's criteria.
-document.getElementById("grow").addEventListener("submit", treeGrow);
+document.getElementById("howHigh").addEventListener("keypress", enterKeyListener);
+document.getElementById("character").addEventListener("keypress", enterKeyListener);
+document.getElementById("growTreeButton").addEventListener("click", treeGrow);
 
+//only executes treeGrow function if the enter key is pressed
+function enterKeyListener(key) {
+	if(key.keyCode === 13)	{
+		treeGrow();
+	}
+}
